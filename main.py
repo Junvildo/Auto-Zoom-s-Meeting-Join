19d8c92f2040a8861cce8415101c3316e5f30840
Trần Nguyễn Tiến lộc <48644569+Junvildo@users.noreply.github.com>
2021-12-17 22:24:37 +0700

    First Commit
    Version 0.0

---
+++ main.py
@@ -0,0 +1,76 @@
+import schedule
+import time
+import os
+import pandas as pd
+import pyautogui as pyAu
+from datetime import datetime
+import pyperclip
+
+ScheduleFile=('Your CSV File Path') #Your CSV File Path. If error, add r before ''
+ZoomLocation=('Your Zoom Path') #Your Zoom Path. If error, add r before ''
+DelayTime=10 #Delay Between Step
+
+DataFrame=pd.read_csv(ScheduleFile,sep=',')
+
+def JobParting(Date):
+    Jobs=DataFrame.loc[DataFrame['Day']==Date]
+    Jobs.reset_index(drop=True,inplace=True)
+    Jobs.sort_values('Time')
+    return Jobs
+
+def AvailableJobs(Jobs):
+    if len(Jobs)==0:
+        return False
+    else:
+        return True
+
+def job(Jobs,index):
+    os.startfile(ZoomLocation)
+    time.sleep(DelayTime)
+    x,y=pyAu.locateCenterOnScreen('JoinButton.png')
+    pyAu.click(x,y)
+    time.sleep(DelayTime)
+    pyAu.write(str(Jobs.loc[index,'RoomID']))
+    pyAu.press('tab',presses=2)
+    pyAu.hotkey('ctrl','a')
+    pyperclip.copy(str(Jobs.loc[index,'ParticipantName']))
+    time.sleep(DelayTime)
+    pyAu.hotkey("ctrl", "v")
+    pyAu.press('enter')
+    time.sleep(DelayTime)
+    pyAu.write(str(Jobs.loc[index,'RoomPassword']))
+    pyAu.press('enter')
+    
+def JobCall(Jobs,index,day):
+    if day=='Monday':
+        schedule.every().monday.at(str(Jobs.loc[index,'Time'])).do(job,Jobs,index)
+    elif day=='Tuesday':
+        schedule.every().tuesday.at(str(Jobs.loc[index,'Time'])).do(job,Jobs,index)
+    elif day=='Wednesday':
+        schedule.every().wednesday.at(str(Jobs.loc[index,'Time'])).do(job,Jobs,index)
+    elif day=='Thurday':
+        schedule.every().thursday.at(str(Jobs.loc[index,'Time'])).do(job,Jobs,index)
+    elif day=='Friday':
+        schedule.every().friday.at(str(Jobs.loc[index,'Time'])).do(job,Jobs,index)
+    elif day=='Saturday':
+        schedule.every().saturday.at(str(Jobs.loc[index,'Time'])).do(job,Jobs,index)
+    else:
+        schedule.every().sunday.at(str(Jobs.loc[index,'Time'])).do(job,Jobs,index)
+
+
+num=0
+Today=datetime.now().strftime('%A')
+TodayJobs=JobParting(Today)
+if AvailableJobs(Jobs=TodayJobs):
+    while num<len(TodayJobs):
+        JobCall(Jobs=TodayJobs,index=num,day=Today)
+        num+=1
+else:
+    print('Not thing to do today. Quiting Now...')
+    quit()
+
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
+
